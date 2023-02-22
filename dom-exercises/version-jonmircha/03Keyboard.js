792e13b6a1559e01693644e62b18448403724cad
Kevin <kevin.paez1010@gmail.com>
2023-02-22 11:26:45 -0600

    keyboard events with limit stage

--- dom-exercises/version-jonmircha/03Keyboard.js
+++ dom-exercises/version-jonmircha/03Keyboard.js
@@ -1,13 +1,67 @@
 const d = document;
 
+let x = 0,
+  y = 0;
+
+export function moveBall(e, ball, stage) {
+  const $ball = document.querySelector(ball);
+  const $stage = document.querySelector(stage);
+  const limitBall = $ball.getBoundingClientRect();
+  const limitStage = $stage.getBoundingClientRect();
+
+  // console.log(limitBall);
+  // console.log(limitStage);
+
+  // console.log(e.keyCode);
+
+  // const move = (direction) => {};
+
+  switch (e.keyCode) {
+    case 37:
+      // move("left");
+      if (limitBall.left > limitStage.left) {
+        e.preventDefault();
+        x--;
+      }
+      break;
+    case 38:
+      // y--;
+      if (limitBall.top > limitStage.top) {
+        e.preventDefault();
+        y--;
+      }
+      // move("up");
+      break;
+    case 39:
+      // move("right");
+      if (limitBall.right < limitStage.right) {
+        e.preventDefault();
+        x++;
+      }
+
+      break;
+    case 40:
+      if (limitBall.bottom < limitStage.bottom) {
+        e.preventDefault();
+        y++;
+      }
+      // move("down");
+      break;
+
+    default:
+      break;
+  }
+  $ball.style.transform = `translate(${x * 10}px, ${y * 10}px)`;
+}
+
 export function shortcut(e) {
-  console.log(`Type: ${e.type}`);
-  console.log(`key: ${e.key}`);
-  console.log(`keyCode: ${e.keyCode}`);
-  console.log(`ctrl: ${e.ctrlKey}`);
-  console.log(`alt: ${e.altKey}`);
-  console.log(`shift: ${e.shiftKey}`);
-  console.log(e);
+  // console.log(`Type: ${e.type}`);
+  // console.log(`key: ${e.key}`);
+  // console.log(`keyCode: ${e.keyCode}`);
+  // console.log(`ctrl: ${e.ctrlKey}`);
+  // console.log(`alt: ${e.altKey}`);
+  // console.log(`shift: ${e.shiftKey}`);
+  // console.log(e);
 
   if (e.key === "a" && e.altKey) {
     alert("You have triggered an alert with the keyboard");
--- dom-exercises/version-jonmircha/main.js
+++ dom-exercises/version-jonmircha/main.js
@@ -1,6 +1,6 @@
 import hamburgerMenu from "./01Hamburger_menu.js";
 import { digitalClock, alarm } from "./02ClockAlarm.js";
-import { shortcut } from "./03Keyboard.js";
+import { moveBall, shortcut } from "./03Keyboard.js";
 
 const d = document;
 
@@ -12,4 +12,6 @@ d.addEventListener("DOMContentLoaded", (e) => {
 
 d.addEventListener("keydown", (e) => {
   shortcut(e);
+  moveBall(e, ".ball", ".stage");
+  // console.log("hi");
 });
